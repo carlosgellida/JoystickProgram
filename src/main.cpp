6687fb0c90637589a83bf58726232fe3c8fd2608
gellidao <gellidao@gmail.com>
2021-07-01 21:50:29 -0500

    It result imposible to try to obtain an absolute orientation

--- src/main.cpp
+++ src/main.cpp
@@ -13,11 +13,12 @@ void setup()
   Wire.begin(); // Iniciar I2C
 
   taskWifi() ; // Inicializar WiFi and websocket client
+  Serial.println("Don't move the device for some seconds");
 
   delay(2000);
 
   InitializeMPU(); // Call imu.begin() to verify communication and initialize
-  
+  Serial.println("MPU has been correctly initialized!");
 }
 
 void loop() 
